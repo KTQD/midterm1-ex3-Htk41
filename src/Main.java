cd1755191ba52776067584c678cbf5068723fdc7
Htk41 <125663636+Htk41@users.noreply.github.com>
2024-03-14 08:46:13 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
         
         try {
-            // Đọc nội dung từ file input.txt
             BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
             StringBuilder content = new StringBuilder();
             String line;
@@ -14,10 +13,8 @@ public class Main {
             }
             reader.close();
 
-            // Thay thế từ "Nha Trang" thành "Vũng Tàu" trong nội dung đọc được
             String replacedContent = content.toString().replaceAll("Nha Trang", "Vũng Tàu");
 
-            // Ghi nội dung đã thay thế vào file output.txt
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
             writer.write(replacedContent);
             writer.close();
